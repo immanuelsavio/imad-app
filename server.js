d65b8b659672fd0f4c241a78e6558cc8aab42338
immanuelsavio <30970161+immanuelsavio@users.noreply.github.com>
2017-08-16 11:48:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.get('/article-two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
-app.get("/article-three" , function(req,res) { 
-    res.send("Article three is requested and will be served here");
+aapp.get('/article-three', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
