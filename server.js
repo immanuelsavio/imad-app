c06f48515538a980c97398a5a2f2de797a57cec4
immanuelsavio <30970161+immanuelsavio@users.noreply.github.com>
2017-08-21 23:46:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,10 +85,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:ArticleName', function (req, res) {
+app.get('/:articleName', function (req, res) {
+    var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
     
-    var articleName = req.params.articleName;
+    
     
     
 });
