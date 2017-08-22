15a274ef50c199dc2ae82f216234a4bce5195c38
immanuelsavio <30970161+immanuelsavio@users.noreply.github.com>
2017-08-22 22:20:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,10 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML = 'NewValue';
-var img = document.getElementById('madi');
-
-var marginLeft = 0;
-function moveRight(){
-    marginLeft  = marginLeft + 1;
-    img.style.marginLeft = marginLeft + "px";   
-}
-madi.onclick = function() {
-   var interval = setInterval(moveRight , 50);
+var button = document.getElementById("counter");
+var counter=0;
+button.onclick = function(){
     
+   
+    counter = counter + 1;
+     
+    var span = document.getElementById("count1");
+    span.innerHTML = counter.toString();
 };
