2a7d596727e4bb817729a385fcba3f6296abc0d8
immanuelsavio <30970161+immanuelsavio@users.noreply.github.com>
2017-08-22 19:45:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,7 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'NewValue';
 var element = document.getElementById('madi');
+var interval = setInterval(moveleft , 100);
 madi.onclick = function() {
     madi.style.marginleft = "100px";
     
