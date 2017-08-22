f1f67563d6bf01dea4a89ce0579f88b18fe6072a
immanuelsavio <30970161+immanuelsavio@users.noreply.github.com>
2017-08-22 19:41:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'NewValue';
-var element = document.getElementById('madi');
-madi.onclick = function() {
-    madi.style.marginleft = "100px";
+var element = document.getElementById('img');
+img.onclick = function() {
+    img.style.marginleft = "100px";
     
 };
