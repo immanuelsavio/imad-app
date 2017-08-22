2135aac77cf21f79b38375b9b969a242ddd00e4e
immanuelsavio <30970161+immanuelsavio@users.noreply.github.com>
2017-08-22 22:24:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,6 @@ button.onclick = function(){
    
     counter = counter + 1;
      
-    var span = document.getElementById("count1");
+    var span = document.getElementById("count");
     span.innerHTML = counter.toString();
 };
