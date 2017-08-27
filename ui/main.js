34d9adcf846e7b6710f6ce215609831e25231f01
immanuelsavio <30970161+immanuelsavio@users.noreply.github.com>
2017-08-27 22:45:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,7 +24,16 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
-  var names = ["name1" , "name2" , "name3"];
+    
+       var request = new XMLHttpRequest();
+   
+   
+   request.onreadystatechange = function(){
+    if(request.readyState===XMLHttpRequest.DONE){
+        
+        if(request.status===200){
+                var names = request.responseText;
+                JSON.parse('names')
   var ist = '';
   for(var i=0;i<names.length;i++)
     {
@@ -36,4 +45,12 @@ submit.onclick = function() {
     var ul = document.getElementById('namelist');
     ul.innerHTML = list
     
+          }
+    }
+   };
+   
+   request.open("GET", "http://immanuelsavio.imad.hasura-app.io/submit-name?name=" + name, true);
+   request.send(null);
+   
+
 };
