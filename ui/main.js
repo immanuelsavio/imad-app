25d64e71f657c39c1651ed2a10957aa76b122040
immanuelsavio <30970161+immanuelsavio@users.noreply.github.com>
2017-08-27 20:43:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,7 @@ button.onclick = function(){
     }
    };
    
-   
-   request.open('GET' , 'http://immanuelsavio.imad.hasura-app.io/counter' , true);
+   request.open("GET", "http://immanuelsavio.imad.hasura-app.io/counter", true);
    request.send(null);
+   
 };
