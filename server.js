ada58555c36403db9e1a439aeacc058f07777ead
immanuelsavio <30970161+immanuelsavio@users.noreply.github.com>
2017-08-27 22:59:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var crypto = require('crypto');
 
 var app = express();
 app.use(morgan('combined'));
@@ -84,6 +85,20 @@ function createTemplate(data) {
 
 app.get('/favicon.ico', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
+});
+
+
+function hash(input, salt){
+    
+    var hashed = crypto.pbkdf2Sync(input, salt , 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+
+app.get("/hash/:input" , function(req,res){
+   
+   var hashedString = hash(req.params.input, 'this-is-some-random-string');
+   res.send(hashedString);
+    
 });
 
 app.get('/', function (req, res) {
